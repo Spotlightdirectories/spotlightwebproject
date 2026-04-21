840fe6aaa34e73255270887d031509615c44dbc5
cyril agobe <spotlightdirectoriesmap@gmail.com>
2026-04-21 17:16:29 +0100

    getlisted page async error fixed

--- getlisted.js
+++ getlisted.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
 // -----------------------------
 // CAPTURE REFERRAL CODE (ENTRY PAGE)
