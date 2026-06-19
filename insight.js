a6d1c75bcaaf493d5cae9630f1164b9b2a2410ec
cyril agobe <spotlightdirectoriesmap@gmail.com>
2026-06-19 18:54:15 +0100

    Insights page being created

---
+++ insight.js
@@ -0,0 +1,22 @@
+const backBtn =
+
+document.getElementById(
+"backToDashboardBtn"
+);
+
+if (backBtn) {
+
+backBtn.addEventListener(
+
+"click",
+
+function(){
+
+window.location.href =
+"vendordashboard.html";
+
+}
+
+);
+
+}
